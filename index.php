ba4f79e0e8193b4843786fd38f7ab7dcc2d4d273
iliatanev <itanev@devrix.com>
2020-04-07 15:07:00 +0300

    Blog page changes

--- index.php
+++ index.php
@@ -15,10 +15,18 @@ get_header(); ?>
   <div class="container container--narrow page-section">
   <?php 
     $wp_query = new WP_Query(array('posts_per_page'=>-1));
-    
+
     while ($wp_query->have_posts()) : $wp_query->the_post();?>
-      <h3><?php the_title();?></h3>
-      <p><?php the_content(); ?></p>   
+      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+      
+      <div class="metabox">
+        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('d.m.Y'); ?> in <?php echo get_the_category_list(', ') ?></p>
+      </div>
+
+      <div class="generic-content">
+        <?php the_excerpt(); ?>
+        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+      </div>
     <?php endwhile;
   ?>
 
