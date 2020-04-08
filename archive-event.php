96fed9b93fdd69612d2b3c385a33f37c8c2ac2c1
iliatanev <itanev@devrix.com>
2020-04-08 14:55:31 +0300

    Fixing the Archive-event

--- archive-event.php
+++ archive-event.php
@@ -14,19 +14,24 @@ get_header(); ?>
 
   <div class="container container--narrow page-section">
   <?php 
-    $wp_query = new WP_Query(array('posts_per_page'=>-1));
+   // $wp_query = new WP_Query(array('posts_per_page'=>-1));
 
-    while ( $wp_query -> have_posts() ) : $wp_query -> the_post();?>
-      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+    while ( have_posts() ) : the_post(); ?>
+      <!-- <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> -->
       
-      <div class="metabox">
-        <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('d.m.Y'); ?> in <?php echo get_the_category_list(', ') ?></p>
-      </div>
-
-      <div class="generic-content">
-        <?php the_excerpt(); ?>
-        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
-      </div>
+      <div class="event-summary">
+          <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
+            <span class="event-summary__month"><?php 
+              $eventDate = new DateTime( get_field( 'event_date' ) );
+              echo $eventDate -> format( 'M' );
+            ?></span>
+            <span class="event-summary__day"><?php echo $eventDate -> format( 'd' ); ?></span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
+            <p><?php echo wp_trim_words( get_the_content(), 15 ) ?><a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
     <?php endwhile;
     
   echo paginate_links();
