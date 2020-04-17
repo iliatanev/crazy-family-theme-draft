4a594c89c044cb61c8e1e31b466e38564fb56d12
iliatanev <itanev@devrix.com>
2020-04-17 14:56:08 +0300

    Code refactoring

--- functions.php
+++ functions.php
@@ -1,5 +1,36 @@
 <?php
 
+function pageBanner( $args = NULL){ //this makes this argument optional
+    
+    if ( !$args['title'] ) {
+          $args['title'] = get_the_title();
+    }
+
+    if ( !$args['subtitle'] ) {
+          $args['subtitle'] = get_field( 'page_banner_subtittle' );
+    }
+
+    if ( !$args['photo'] ) {
+        if ( get_field( 'page_banner_background_image' ) ) {
+            $args['photo'] = get_field( 'page_banner_background_image' );
+        } else {
+            $args[ 'photo' ] = get_theme_file_uri( '/images/ocean.jpg' );
+        }
+    }
+    ?>
+
+    <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url( <?php echo $args['photo'] ?> );"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle'] ?></p>
+      </div>
+    </div>  
+  </div>
+
+<?php }
+
 function crazyfamily_files() {
 
     wp_enqueue_script( 'main-family-js', get_theme_file_uri( '/js/scripts-bundled.js' ), NULL, '1.0', true );
--- page.php
+++ page.php
@@ -4,18 +4,14 @@ get_header();
 
 while (have_posts()) {
     
-    the_post(); ?>
+    the_post(); 
+    pageBanner( array(
+      //'title' => 'Hello there this is the title',
+      'subtitle' => 'This is the subtitle',
+      'photo' => 'https://www.imom.com/wp-content/uploads/2014/06/family_fun_5_great_family_field_trips.jpg'
+    ) );
+    ?>
     
-    <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ); ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title() ?></h1>
-      <div class="page-banner__intro">
-        <p>Learn how the school of your dreams got started.</p>
-      </div>
-    </div>  
-  </div>
-
   <div class="container container--narrow page-section">
 
     <?php 
